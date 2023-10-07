0143cd27f08d27f551f13e4f510d6198dfd21135
IvanBanifatsyeu <gavrik2010@yahoo.com>
2023-10-07 18:48:04 +0300

    Nested routing in React Router 6

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,8 +1,11 @@
 import { Link, useMatch } from "react-router-dom";
 
 const CustomLink = ({ children, to, ...props }) => {
-	const match = useMatch(to);
-
+	const match = useMatch({
+		path: to,
+		end: to.length === 1,
+	});
+	console.log(match);
 	return (
 		<Link
 			to={to}
--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { Link, Outlet } from "react-router-dom";
 
 const Aboutpage = () => {
 	return (
 		<div>
 			<h1>About us</h1>
 			<p>This is a demo website about React-router-dom library</p>
+
+			<ul>
+				<li>
+					<Link to="contacts">Our Contacts</Link>
+				</li>
+				<li>
+					<Link to="team">Our Team</Link>
+				</li>
+			</ul>
+			<Outlet />
 		</div>
 	);
 };
